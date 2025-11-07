f48cf067d87f7f47c71ad9b01a740ac5485471c8
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-07 16:23:30 +0100

    Updated QuickStatsCells.tsx

--- src/components/PlayerProfile/QuickStatsCells.tsx
+++ src/components/PlayerProfile/QuickStatsCells.tsx
@@ -123,7 +123,7 @@ export function QuickStatsCells({ player, inventory, activeTooltip, setActiveToo
         className="flex flex-col items-center justify-center cursor-pointer relative"
         onClick={() => setActiveTooltip && setActiveTooltip(activeTooltip === 'ac' ? null : 'ac')}
       >
-       <div className="relative w-20 h-20 lg:w-20 lg:h-20"> 
+       <div className="relative w-16 h-16 lg:w-20 lg:h-20">  
           <ShieldIcon className="absolute inset-0 w-full h-full text-gray-400 stroke-[1]" />
           <div className="absolute inset-0 flex items-center justify-center text-xl font-bold text-gray-100">
             {totalAC}
