219f8994b720deeb19585ef77453727c60896264
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-08 23:16:39 +0100

    Updated StandaloneSkillsSection.tsx

--- src/components/StandaloneSkillsSection.tsx
+++ src/components/StandaloneSkillsSection.tsx
@@ -130,16 +130,14 @@ export function StandaloneSkillsSection({ player, onSkillClick }: StandaloneSkil
   };
 
   return (
-    <div className="bg-gray-800/30 rounded-lg border border-gray-700 p-4 w-full">
-      <SkillsTable
-        allSkills={allSkills}
-        editing={false}
-        expertiseLimit={0}
-        handleProficiencyChange={() => {}}
-        handleExpertiseChange={() => {}}
-        rollSkillCheck={handleSkillClick}
-        statsJackOfAllTrades={player.stats?.jack_of_all_trades || false}
-      />
-    </div>
+    <SkillsTable
+      allSkills={allSkills}
+      editing={false}
+      expertiseLimit={0}
+      handleProficiencyChange={() => {}}
+      handleExpertiseChange={() => {}}
+      rollSkillCheck={handleSkillClick}
+      statsJackOfAllTrades={player.stats?.jack_of_all_trades || false}
+    />
   );
 }
