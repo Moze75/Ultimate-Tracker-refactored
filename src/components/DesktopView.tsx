a0997778c727bafca62fb30a449d4b0f6c78c53f
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-08 23:44:57 +0100

    Updated DesktopView.tsx

--- src/components/DesktopView.tsx
+++ src/components/DesktopView.tsx
@@ -7,7 +7,7 @@ import { HPManagerConnected } from './Combat/HPManagerConnected';
 import { HorizontalAbilityScores } from './HorizontalAbilityScores';
 import { StandaloneSkillsSection } from './StandaloneSkillsSection';
 import { TabbedPanel } from './TabbedPanel';
-import { DiceRollerLazy } from './DiceRollerLazy';
+import { DiceRoller } from './DiceRoller';
 import { ConcentrationCheckModal } from './Combat/ConcentrationCheckModal';
 import { useResponsiveLayout } from '../hooks/useResponsiveLayout';
 
