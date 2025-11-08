0416719337f1355025f6cabf3f5b7922feae72d9
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-08 23:45:11 +0100

    Updated DesktopView.tsx

--- src/components/DesktopView.tsx
+++ src/components/DesktopView.tsx
@@ -167,7 +167,7 @@ export function DesktopView({
         </div>
       </div>
 
-<DiceRollerLazy
+<DiceRoller
   isOpen={diceRoll !== null}
   onClose={() => setDiceRoll(null)}
   rollData={diceRoll ? {
