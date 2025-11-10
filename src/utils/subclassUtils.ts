bca585ea18adc3f38dd3e8936269ae21e17e0137
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-10 23:50:00 +0100

    Updated subclassUtils.ts

--- src/utils/subclassUtils.ts
+++ src/utils/subclassUtils.ts
@@ -46,7 +46,7 @@ export const SUBCLASS_ALIASES: Record<string, string[]> = {
   'credo des elements': ['Crédo des Éléments', 'Credo des Elements', 'Way of the Four Elements'],
   'credo de la misericorde': ['Crédo de la Miséricorde', 'Credo de la Misericorde', 'Way of Mercy'],
   // NOTE: principale étiquette mise en forme ASCII (sans apostrophe) pour correspondre au nom de fichier GitHub
-  'credo de l ombre': ['Credo de l Ombre', "Crédo de l'Ombre", 'Crédo de l’Ombre', 'Crédo de l Ombre', 'Shadow'],
+  'credo de l ombre': ['Crédo de l Ombre', "Crédo de l'Ombre", 'Crédo de l’Ombre', 'Credo de l Ombre', 'Shadow'],
   'credo de la paume': ['Crédo de la Paume'],
 
   // Occultiste (Warlock)
