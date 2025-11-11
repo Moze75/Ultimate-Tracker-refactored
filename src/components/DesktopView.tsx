2430a0bd655a8b7a8d1fc4a79565e62211d2b583
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-11 16:29:55 +0100

    Updated DesktopView.tsx

--- src/components/DesktopView.tsx
+++ src/components/DesktopView.tsx
@@ -81,7 +81,7 @@ export function DesktopView({
   };
 
   // Hauteur de la bande grise (agrandie)
-  const headerBandHeight = 600; // en pixels
+  const headerBandHeight = 500; // en pixels
 
   return (
     <>
