aed69eb0335d94dbc032f3cce620debf0cac3d24
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-11 16:35:54 +0100

    Updated DesktopView.tsx

--- src/components/DesktopView.tsx
+++ src/components/DesktopView.tsx
@@ -93,7 +93,7 @@ export function DesktopView({
               className="min-h-screen"
               style={{
                 width: '100%', // Réduit de moitié (de 3600px à 1800px)
-               backgroundImage: `linear-gradient(to bottom, rgba(17, 24, 39, 1) 0px, rgba(17, 24, 39, 0.7) 100px, transparent 250px), url(/background/bgfan.jpg)`,
+                backgroundImage: 'url(/background/bgfan.png)',    
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: 'center 180px', // Descendu davantage (de 120px à 180px)
                 backgroundSize: 'cover',
