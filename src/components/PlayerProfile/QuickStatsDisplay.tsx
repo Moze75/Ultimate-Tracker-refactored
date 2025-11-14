e98ec7ba188dd707c7bc4da586bfb9c9495de916
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-14 23:07:43 +0100

    Updated QuickStatsDisplay.tsx

--- src/components/PlayerProfile/QuickStatsDisplay.tsx
+++ src/components/PlayerProfile/QuickStatsDisplay.tsx
@@ -167,7 +167,7 @@ export function QuickStatsDisplay({ player, inventory, activeTooltip, setActiveT
       </div>
 
       {/* Vitesse */}
-      <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center pt-2">
         <div
           className="relative w-16 h-10 -mt-2 -mb-1 group cursor-pointer"
           onClick={() => setActiveTooltip(activeTooltip === 'speed' ? null : 'speed')}
