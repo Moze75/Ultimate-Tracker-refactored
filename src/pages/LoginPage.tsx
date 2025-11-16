7a14149373bec5e581c926af7450bbf0f7fdec7d
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-16 21:28:01 +0100

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -346,8 +346,8 @@ const toggleForgotPassword = () => {
               0 0 40px rgba(255, 255, 255, 0.2)
             `
           }}>
+            <div className="leading-tight">La Compagnon</div>
             <div className="leading-tight">D&D</div>
-            <div className="leading-tight">Compagnon App</div>
           </h1>
           <p className="text-gray-200 mb-2" style={{
             textShadow: '0 0 10px rgba(255, 255, 255, 0.3)'
