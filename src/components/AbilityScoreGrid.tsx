c3454867656ca5d1253c5e3a443fb6aa60ea1b21
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-20 23:01:32 +0100

    Updated AbilityScoreGrid.tsx

--- src/components/AbilityScoreGrid.tsx
+++ src/components/AbilityScoreGrid.tsx
@@ -54,7 +54,7 @@ export function AbilityScoreGrid({
                 onClick={() => !editing && rollAbilityCheck(ability)}
                 title={!editing ? `Cliquer pour lancer 1d20+${ability.modifier}` : ''}
               >
-                <div className="absolute top-13 left-0 right-0 flex flex-col items-center pointer-events-none">
+                <div className="absolute top-6 left-0 right-0 flex flex-col items-center pointer-events-none">
 <h4 className="text-[9px] font-normal text-gray-100 uppercase tracking-wide">
   {ability.name}
 </h4>
