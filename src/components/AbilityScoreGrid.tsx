94465ea643f75ba3085d5dc7653463c8c59ab5b6
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-20 22:09:49 +0100

    Updated AbilityScoreGrid.tsx

--- src/components/AbilityScoreGrid.tsx
+++ src/components/AbilityScoreGrid.tsx
@@ -32,7 +32,7 @@ export function AbilityScoreGrid({
 
   return (
     <div>
-   <div key={ability.name} className="flex flex-col items-center mt-4"> 
+      <div className="grid grid-cols-3 gap-4 mb-6"> 
         {abilities.map((ability, abilityIndex) => {
           const equipmentBonuses = calculateEquipmentBonuses();
           const equipmentBonus = equipmentBonuses[ability.name as keyof typeof equipmentBonuses] || 0;
