f98643d251989c3c36164d0b503d14e6ce4f1c00
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-11-30 16:22:23 +0100

    Updated DesktopView.tsx

--- src/components/DesktopView.tsx
+++ src/components/DesktopView.tsx
@@ -263,6 +263,7 @@ export function DesktopView({
   )}
 </div>
             </div> 
+            </div>
 
             {/* Grille Skills + TabbedPanel */}
             <div className="grid grid-cols-12 gap-4">
