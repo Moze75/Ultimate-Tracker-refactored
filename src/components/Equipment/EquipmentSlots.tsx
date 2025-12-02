1300bf4057b444c18af246f8eb70955f38d80916
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-12-02 20:44:30 +0100

    Updated EquipmentSlots.tsx

--- src/components/Equipment/EquipmentSlots.tsx
+++ src/components/Equipment/EquipmentSlots.tsx
@@ -36,22 +36,18 @@ interface EquipmentSlotProps {
   inventory?: InventoryItem[];
 }
 
-export function EquipmentSlots({
-  armor,
-  shield,
-  weaponsSummary,
-  potionText,
-  jewelryText,
-  bag,
-  bagText,
-  inventory,
-  equippedWeaponsCount,
-  onOpenInventoryModal,
-  onToggleFromSlot,
+export function EquipmentSlot({
+  icon,
+  position,
+  equipment,
+  type,
+  onRequestOpenList,
+  onToggleEquipFromSlot,
   onOpenEditFromSlot,
-  onOpenWeaponsModal,
-  onOpenBagModal
-}: EquipmentSlotsProps) {
+  isEquipped,
+  onOpenWeaponsManageFromSlot,
+  inventory
+}: EquipmentSlotProps) {
   return (
     <div className="stat-card">
       <div className="stat-header flex items-center gap-3">
