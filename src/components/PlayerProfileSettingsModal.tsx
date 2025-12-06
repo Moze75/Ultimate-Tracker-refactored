6456c92234fe730383604ab8a1dd7b567c284661
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-12-06 15:23:12 +0100

    Updated PlayerProfileSettingsModal.tsx

--- src/components/PlayerProfileSettingsModal.tsx
+++ src/components/PlayerProfileSettingsModal.tsx
@@ -502,6 +502,8 @@ const autoAC = hasArmorEquipped
   ? (10 + dexMod)  // Avec armure, la formule d'armure prendra le dessus à l'affichage
   : calculateUnarmoredAC(player);
 
+
+
 setAcField(acInitial > 0 ? String(acInitial) : String(autoAC));
     setInitField(initInitial !== undefined && initInitial !== null ? String(initInitial) : String(dexMod));
     setSpeedField(speedInitial > 0 ? String(speedInitial).replace('.', ',') : String(9));
