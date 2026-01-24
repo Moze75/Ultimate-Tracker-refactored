836f27799d2ff45821f96e3f15698702578b40f8
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-01-24 20:44:49 +0100

    Updated CharacterSelectionPage.tsx

--- src/pages/CharacterSelectionPage.tsx
+++ src/pages/CharacterSelectionPage.tsx
@@ -41,7 +41,7 @@ const PENDING_PLAN_KEY = 'pending_plan_selection'; // Clé utilisée dans la Hom
 
 const BG_URL =
   (import.meta as any)?.env?.VITE_SELECTION_BG_URL ||
-  'https://yumzqyyogwzrmlcpvnky.supabase.co/storage/v1/object/public/static/tmpoofee5sh.png';
+  'https://pub-34f7ade8969e4687945b58e1d1b80dd8.r2.dev/static/tmpoofee5sh.png';
 
 type CreatorModalProps = {
   open: boolean;
