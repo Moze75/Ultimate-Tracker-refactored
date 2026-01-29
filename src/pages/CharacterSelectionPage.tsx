0dd756a98b50c2caa5b26c868a7d39d956a87d1d
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-01-29 10:47:28 +0100

    Updated CharacterSelectionPage.tsx

--- src/pages/CharacterSelectionPage.tsx
+++ src/pages/CharacterSelectionPage.tsx
@@ -190,11 +190,7 @@ useEffect(() => {
     }
     hasInitializedRef.current = true;
 
-  // ✅ Modal de bienvenue à la première connexion après inscription
-  if (localStorage.getItem('ut:show-first-welcome')) {
-    setShowFirstWelcome(true);
-    localStorage.removeItem('ut:show-first-welcome');
-  }
+
     
     // ✅ GESTION DE LA REDIRECTION APRÈS LOGIN
     // On vérifie si l'utilisateur avait cliqué sur un plan avant de se loguer
