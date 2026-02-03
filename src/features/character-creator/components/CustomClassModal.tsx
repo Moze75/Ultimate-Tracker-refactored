59d4e2e7a15c00bd2cd6315b2920bf87cff07521
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-03 12:02:30 +0100

    Updated CustomClassModal.tsx

--- src/features/character-creator/components/CustomClassModal.tsx
+++ src/features/character-creator/components/CustomClassModal.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { createPortal } from 'react-dom';
-import Button from './ui/Button';
+import Button from './ui/Button'; 
 import Input from './ui/Input';
 import Card, { CardContent, CardHeader } from './ui/Card';
 import {
