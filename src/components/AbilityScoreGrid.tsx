63cd7bc7efef52c3babfaa7cbae8459061b11f4f
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-03 14:11:13 +0100

    Updated AbilityScoreGrid.tsx

--- src/components/AbilityScoreGrid.tsx
+++ src/components/AbilityScoreGrid.tsx
@@ -72,10 +72,10 @@ export function AbilityScoreGrid({
 </h4>
                 </div>
 
-                {equipmentBonus !== 0 && (
+                {totalBonus !== 0 && (
                   <div className="absolute -top-0 left-1/2 transform -translate-x-1/2 pointer-events-none">
                     <div className="text-[10px] text-green-400 leading-none whitespace-nowrap bg-gray-900/80 px-2 py-0.5 rounded">
-                      ({baseModifier >= 0 ? '+' : ''}{baseModifier} {equipmentBonus > 0 ? '+' : ''}{equipmentBonus})
+                      ({baseModifier >= 0 ? '+' : ''}{baseModifier} {totalBonus > 0 ? '+' : ''}{totalBonus})
                     </div>
                   </div>
                 )}
