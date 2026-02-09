2c6177f6b59e7f346d798fb515c0ce1aaca1a5ac
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-09 17:27:44 +0100

    Updated DiceSettingsModal.tsx

--- src/components/DiceSettingsModal.tsx
+++ src/components/DiceSettingsModal.tsx
@@ -642,7 +642,7 @@ function BackgroundTab({
     { url: '/fondecran/Toits_copy.png', name: 'Toits copy', type: 'image' as const },
     { url: 'https://pub-34f7ade8969e4687945b58e1d1b80dd8.r2.dev/static/backscreen/Underwater.png', name: 'Underwater', type: 'image' as const },
     { url: 'https://pub-34f7ade8969e4687945b58e1d1b80dd8.r2.dev/static/backscreen/War.png', name: 'War', type: 'image' as const },
-    { url: '/fondecran/War_2.png', name: 'War 2', type: 'image' as const },
+    { url: 'https://pub-34f7ade8969e4687945b58e1d1b80dd8.r2.dev/static/backscreen/War%202.png', name: 'War 2', type: 'image' as const },
     { url: '/fondecran/War_3.png', name: 'War 3', type: 'image' as const },
     { url: '/fondecran/Ascendance.jpg', name: 'Ascendance', type: 'image' as const },
   ];
