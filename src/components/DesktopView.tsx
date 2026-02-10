2eb0d259a4722e35e271bf57e5e9995137ab8e1a
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-10 15:30:06 +0100

    Updated DesktopView.tsx

--- src/components/DesktopView.tsx
+++ src/components/DesktopView.tsx
@@ -255,7 +255,7 @@ export function DesktopView({
               </div>
 
               <div className="col-span-8 flex">
-                <div className="frame-card p-4 w-full flex flex-col max-h-[880px]">
+                <div className="frame-card frame-card--tex2 p-4 w-full flex flex-col max-h-[880px]">
                   <TabbedPanel
                     player={player}
                     inventory={inventory}
