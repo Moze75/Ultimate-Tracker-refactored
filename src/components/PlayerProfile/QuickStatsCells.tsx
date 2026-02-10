70c5488ed7a4bc6d955d4ef2bdb6d06f2c74a6d9
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-10 17:14:34 +0100

    Updated QuickStatsCells.tsx

--- src/components/PlayerProfile/QuickStatsCells.tsx
+++ src/components/PlayerProfile/QuickStatsCells.tsx
@@ -170,7 +170,7 @@ return (
         </span>
       </div>
       <div 
-        className="text-xs uppercase tracking-wide text-gray-500 -mt-2"
+        className="text-xs uppercase tracking-wide text-gray-500 -mt-13"
         style={{ marginLeft: '0px' }}
       >
         CA
