2dc007d312f3046e5c103ba4932afe0b9d7ba013
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-14 23:19:33 +0100

    Updated GamePage.tsx

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1020,6 +1020,12 @@ const [showCampaignModal, setShowCampaignModal] = useState(false);
                         onOpenCampaigns={() => setShowCampaignModal(true)}
                       /> 
                     </div>
+                  <CampaignPlayerModal
+  open={showCampaignModal}
+  onClose={() => setShowCampaignModal(false)}
+  player={currentPlayer}
+  onUpdate={applyPlayerUpdate}
+/>
                   )}
 
                   {isGridMode && deviceType === 'desktop' ? (
