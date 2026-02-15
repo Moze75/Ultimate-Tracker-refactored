c93408d4f2de1ad0fe3caaa78185fce14e70ebe1
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-15 22:42:22 +0100

    Updated PlayerProfileSettingsModal.tsx

--- src/components/PlayerProfileSettingsModal.tsx
+++ src/components/PlayerProfileSettingsModal.tsx
@@ -929,7 +929,7 @@ useEffect(() => {
       onTouchMove={handleTouchMove}
       onTouchEnd={handleTouchEnd}
     >
-        <div className="max-w-4xl mx-auto p-4 py-8 space-y-6 frame-card frame-card--tex3 frame-card--no-frame">
+        <div className="max-w-4xl mx-auto p-4 py-8 space-y-6 frame-card frame-card--light frame-card--no-frame">
           <div className="flex items-center justify-between mb-2">
             <h2 className="text-2xl font-bold text-gray-100">Paramètres du personnage</h2>
             <button
