542012176606c03cecfad744b5d16d77321d9cb1
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-02-17 12:54:47 +0100

    Updated CombatTab.tsx

--- src/components/CombatTab.tsx
+++ src/components/CombatTab.tsx
@@ -662,6 +662,7 @@ export default function CombatTab({ player, inventory, onUpdate }: CombatTabProp
       setConcentrationDC(dc);
       setShowConcentrationCheck(true);
     }}
+    markLocalUpdate={markLocalUpdate}
   />
 )}
 
