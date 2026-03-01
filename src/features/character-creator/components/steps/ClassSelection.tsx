856454e277289ef8d02f4dceedec07a64ac2755b
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-01 17:49:32 +0100

    Updated ClassSelection.tsx

--- src/features/character-creator/components/steps/ClassSelection.tsx
+++ src/features/character-creator/components/steps/ClassSelection.tsx
@@ -624,19 +624,24 @@ const ClassSelection: React.FC<ClassSelectionProps> = ({
       </div>
 
       <div className="flex flex-col-reverse sm:flex-row sm:justify-between gap-3 pt-8">
-        <Button onClick={onPrevious} variant="secondary" size="lg" className="w-full sm:w-auto sm:min-w-[200px]">
+        <Button
+          onClick={onPrevious}
+          variant="secondary"
+          size="lg"
+          className="w-full sm:w-auto sm:min-w-[200px]"
+        >
           Précédent
         </Button>
         <Button
           onClick={onNext}
-          disabled={!selectedBackground || !selectedEquipmentOption}
+          disabled={!selectedClass}
           size="lg"
           className="w-full sm:w-auto sm:min-w-[200px]"
         >
-          Continuer
+          Continuer 
         </Button>
       </div>
-      
+
       <CardDetailModal
         isOpen={modalCardIndex !== null}
         onClose={() => setModalCardIndex(null)}
