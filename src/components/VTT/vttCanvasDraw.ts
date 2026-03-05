697a0be5c97ba039ec801c402a45040f6e1848dd
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-05 22:12:26 +0100

    Updated vttCanvasDraw.ts

--- src/components/VTT/vttCanvasDraw.ts
+++ src/components/VTT/vttCanvasDraw.ts
@@ -105,12 +105,7 @@ export function drawVTTCanvas(ctx2d: VTTDrawContext): void {
   const myControlledTokens = ctx2d.tokensRef.current.filter(t => {
     if (!t.visible) return false;
     if (curRole !== 'player') return true;
-    // Un joueur ne "contrôle" un token que s'il figure explicitement dans controlledByUserIds
-    // OU s'il en est le ownerUserId. On évite ainsi qu'un token partagé entre plusieurs joueurs
-    // donne sa vision à tous.
-    const isOwner = t.ownerUserId === curUserId;
-    const isController = t.controlledByUserIds?.includes(curUserId) ?? false;
-    return isOwner || isController;
+/ Un joueur ne "contrôle" un token qu
   });
 
   // myVisionTokens = mes tokens qui ont une vision active
