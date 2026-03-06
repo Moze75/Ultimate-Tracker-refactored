f76eaffc0f97dbaada962576a03bc893fc8f9b39
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-06 14:30:09 +0100

    Updated useVTTCanvasEvents.ts

--- src/components/VTT/useVTTCanvasEvents.ts
+++ src/components/VTT/useVTTCanvasEvents.ts
@@ -40,6 +40,7 @@ export interface VTTCanvasRefs {
   onWallUpdatedRef: React.MutableRefObject<((wall: any) => void) | undefined>;
   onWallRemovedRef: React.MutableRefObject<((wallId: string) => void) | undefined>;
     selectedWallPointRef: React.MutableRefObject<{ wallId: string; pointIndex: number } | null>;
+  selectedWallPointsRef: React.MutableRefObject<{ wallId: string; pointIndex: number }[]>;
   onViewportChangeRef: React.MutableRefObject<((vp: { x: number; y: number; scale: number }) => void) | undefined>;
   fogCanvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
   fogBrushRef?: React.MutableRefObject<number>;
