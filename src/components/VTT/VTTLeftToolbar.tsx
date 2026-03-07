122e2a82fb9e6d37598793ce6af3144543e6ef3a
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-07 13:53:22 +0100

    Updated VTTLeftToolbar.tsx

--- src/components/VTT/VTTLeftToolbar.tsx
+++ src/components/VTT/VTTLeftToolbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { MousePointer2, Eye, EyeOff, UserPlus, Cloud, X, RefreshCw, Grid3x3 as Grid3X3, Crosshair, Trash2, Sun, Moon, Fence, Ruler, MonitorPlay, ExternalLink, Copy, Check, RectangleHorizontal, Lock, Unlock, Clock, Sunrise, Sunset, Sparkles, Wind } from 'lucide-react';
+import { MousePointer2, Eye, EyeOff, UserPlus, Cloud, X, RefreshCw, Grid3x3 as Grid3X3, Crosshair, Trash2, Sun, Moon, Fence, Ruler, MonitorPlay, ExternalLink, Copy, Check, RectangleHorizontal, Lock, Unlock, Clock, Sunrise, Sunset, Sparkles, Wind, SlidersHorizontal, } from 'lucide-react';
 import type { VTTWeatherEffect, VTTWeatherType } from '../../types/vtt';
 import type { VTTRole, VTTRoomConfig } from '../../types/vtt';
 
