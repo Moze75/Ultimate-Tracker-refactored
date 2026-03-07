7dfd8ca199d645f8b750f5aed1260075e5ee440e
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-08 00:13:21 +0100

    Updated VTTPage.tsx

--- src/pages/VTTPage.tsx
+++ src/pages/VTTPage.tsx
@@ -813,8 +813,9 @@ export function VTTPage({ session, onBack }: VTTPageProps) {
 
   return (
     <div className="flex flex-col h-screen bg-gray-950 overflow-hidden">
-      <div className="flex flex-1 overflow-hidden">
-        <VTTLeftToolbar 
+      <div className="flex flex-1 overflow-hidden relative">
+        <div className="absolute left-0 top-0 bottom-0 z-30 pointer-events-auto">
+          <VTTLeftToolbar
           role={role}
           activeTool={activeTool}
           fogBrushSize={fogBrushSize}
