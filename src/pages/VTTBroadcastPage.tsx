9381c218486e67d7ab536237e2f1059055e8d910
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-07 18:50:50 +0100

    Updated VTTBroadcastPage.tsx

--- src/pages/VTTBroadcastPage.tsx
+++ src/pages/VTTBroadcastPage.tsx
@@ -78,6 +78,9 @@ export function VTTBroadcastPage({ session, roomId, onBack }: VTTBroadcastPagePr
         break;
       case 'MAP_UPDATED':
         setConfig(prev => ({ ...prev, ...event.config }));
+        if (event.config.savedViewport) {
+          setSavedViewport(event.config.savedViewport);
+        }
         break;
       case 'SCENE_SWITCHED':
         setConfig(event.config);
