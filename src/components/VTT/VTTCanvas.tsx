3dde462791faa1c909a5259e2bc751cb990a9d12
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-08 23:46:41 +0100

    Updated VTTCanvas.tsx

--- src/components/VTT/VTTCanvas.tsx
+++ src/components/VTT/VTTCanvas.tsx
@@ -24,6 +24,7 @@ export const VTTCanvas = forwardRef<VTTCanvasHandle, VTTCanvasProps>(function VT
   onRightClickToken,
   onMapDimensions,
   onDropToken,
+  onDropProp,
   onAddTokenAtPos,
   onResizeToken,
   calibrationPoints,
