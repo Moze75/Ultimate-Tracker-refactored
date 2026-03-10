a510cd1139913a6755e71da192e546dbaa32d0aa
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-10 17:33:56 +0100

    Updated VTTSidebar.tsx

--- src/components/VTT/VTTSidebar.tsx
+++ src/components/VTT/VTTSidebar.tsx
@@ -187,8 +187,7 @@ const visibleTokens = isGM
     // Gestion de la transparence de la sidebar
     // -------------------
     // La sidebar devient un panneau en surimpression au-dessus du canvas.
-<div className="flex flex-col w-56 h-full bg-slate-900/10
-
+<div className="flex flex-col w-56 h-full bg-slate-900/10 backdrop-blur-lg border-l border-white/10 overflow-hidden shadow-2xl"> 
       <div className="flex border-b border-gray-700/60 shrink-0">
    <TabBtn icon={<Users size={14} />} title="Tokens" active={activeTab === 'tokens'} onClick={() => setActiveTab('tokens')} />
 {isGM && (
