54903af506f858e78a86bee01e01537641f2f3ad
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-13 22:11:40 +0100

    Updated VTTCanvas.tsx

--- src/components/VTT/VTTCanvas.tsx
+++ src/components/VTT/VTTCanvas.tsx
@@ -989,7 +989,7 @@ export const VTTCanvas = forwardRef<VTTCanvasHandle, VTTCanvasProps>(function VT
       <canvas
         ref={canvasRef}
         className="absolute inset-0 touch-none"
-        style={{ cursor: isFogTool ? 'none' : (isWallTool || isMeasureTool) ? 'crosshair' : 'default' }}
+               style={{ cursor: isFogTool ? 'none' : (isFogRectTool || isWallTool || isMeasureTool) ? 'crosshair' : 'default' }}
       />
 
       {isDragOver && (
