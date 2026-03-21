7836de9a4b8d0b81308e6efa85ac216f33e5cf3f
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-03-22 00:25:32 +0100

    Updated VTTSidebar.tsx

--- src/components/VTT/VTTSidebar.tsx
+++ src/components/VTT/VTTSidebar.tsx
@@ -284,7 +284,7 @@ const visibleTokens = isGM
     // La poignée doit être ici, HORS du overflow-hidden,
     // sinon le navigateur coupe le positionnement absolu
     // et top:50% ne se calcule que sur la zone visible, pas toute la hauteur.
-    <div className="relative h-full flex-shrink-0" style={{ width: sidebarWidth }}>
+ <div className="relative h-full flex-shrink-0 z-20" style={{ width: sidebarWidth }}>
 
 // Poignée de redimensionnement
       
