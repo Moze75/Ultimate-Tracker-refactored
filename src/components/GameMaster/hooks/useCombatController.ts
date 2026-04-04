a692d46126154ef2ce7cdd37f4e6df4e640fc1d2
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-04-04 23:08:13 +0200

    Updated useCombatController.ts

--- src/components/GameMaster/hooks/useCombatController.ts
+++ src/components/GameMaster/hooks/useCombatController.ts
@@ -1043,6 +1043,7 @@ export function useCombatController({
     handleRollAllInitiative,
     handleRollMonsterInitiativeActive,
     handleLaunchCombat,
+    handleDirectLaunchCombat,
     handleSavePreparation,
     handleSaveEncounter,
     handleEndCombat,
