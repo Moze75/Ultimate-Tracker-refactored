71eead5304540df1dc5763f1312ff127d6d01373
Moze75 <120105440+Moze75@users.noreply.github.com>
2026-04-04 23:08:36 +0200

    Updated VTTCombatTab.tsx

--- src/components/VTT/combat/VTTCombatTab.tsx
+++ src/components/VTT/combat/VTTCombatTab.tsx
@@ -106,6 +106,7 @@ export function VTTCombatTab({
     handleRollAllInitiative,
     handleRollMonsterInitiativeActive,
     handleLaunchCombat,
+    handleDirectLaunchCombat,
     handleSavePreparation,
     handleSaveEncounter,
     handleEndCombat,
